1d10d0a6dd0e352c41546f532479466ef15a5e0f
firstarchive <ba86.work@gmail.com>
2024-10-08 23:05:48 +0700

    ✨ feat: PWA & Styling Enhancements
    - 🔧 Setting PWA => config, manifest
    - 🎨 Setting CSS =>
      -- ❌ Don't right click function (for disable right click)
      -- ✋ No-select
      -- 🚫 No-tap-highlight
    - 🆕 Added font => GraphikTH and setting to use
    - 📦 Create component basic => Header, Footer
    - ⚠️ Note: shadcn CSS framework installation is planned for future use

---
+++ app/components/config/DontRightClick.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect } from "react";
+
+export default function DontRightClick({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
+    // ปิดการใช้งานคลิกขวาทั้งหน้า
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+  return <>{children}</>;
+}
